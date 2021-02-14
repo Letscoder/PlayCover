6a5d23a5df6920b76349c2443480615e80b79da4
Letscoder <write2me4help@gmail.com>
2021-02-14 03:31:24 +0300

    Initial Commit

---
+++ PlayCover/PlayCoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayCoverApp.swift
+//  PlayCover
+//
+//  Created by Alex on 14.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct PlayCoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
